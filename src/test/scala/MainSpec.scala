de6adc2aa256ac97edf408f3c5091e75f7f844f1
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2013-11-06 14:52:45 -0500

    Add specs2 route tests.

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,10 +1,28 @@
 package com.mlh.spraysample
-import org.scalatest._
-import org.scalatest.matchers._
+import org.specs2.mutable.Specification
+import spray.testkit.Specs2RouteTest
+import spray.http.HttpEntity
+import spray.http.ContentTypes
 
-class MainSpec extends FreeSpec with ShouldMatchers {
-  "The spraysample class" - {
-    "should be able to run" in {
+class MainSpec extends Specification with Specs2RouteTest with SpraySampleService {
+  def actorRefFactory = system
+
+  "The spraysample class" should {
+    "List entities" in {
+      Get("/entity") ~> spraysampleRoute ~> check {
+        contentType.mediaType.isApplication must beEqualTo(true)
+        contentType.toString must contain("application/json")
+	// FIXME: Should be able to get response as JObject
+	responseAs[String] === "\"list\""
+      }
+    }
+    "Post an entity" in {
+      // FIXME: Should be able to send entity as JObject
+      Post("/entity", HttpEntity(ContentTypes.`application/json`, """{"json":[ ]}""")) ~> spraysampleRoute ~> check {
+	responseAs[String] must contain("\"I got a response: Ok")
+      }
+    }
+    "Get stats" in {
       pending
     }
   }
