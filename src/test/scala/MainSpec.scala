854d2627834fe9a76907eacd5682be7497679c11
ashawley <aaron.s.hawley@gmail.com>
2013-12-01 01:29:03 -0500

    Bring back ScalaTest for automated tests

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,29 +1,44 @@
 package com.mlh.spraysample
-import org.specs2.mutable.Specification
-import spray.testkit.Specs2RouteTest
+import org.scalatest.FreeSpec
+import org.scalatest.matchers.ShouldMatchers
+import spray.testkit.ScalatestRouteTest
 import spray.http.HttpEntity
 import spray.http.ContentTypes
+import spray.can.server.Stats
 
-class MainSpec extends Specification with Specs2RouteTest with SpraySampleService {
+class MainSpec extends FreeSpec with ShouldMatchers with ScalatestRouteTest with SpraySampleService {
   def actorRefFactory = system
 
-  "The spraysample class" should {
-    "List entities" in {
-      Get("/entity") ~> spraysampleRoute ~> check {
-        contentType.mediaType.isApplication must beEqualTo(true)
-        contentType.toString must contain("application/json")
-	// FIXME: Should be able to get response as JObject
-	responseAs[String] === "\"list\""
+  "The spraysample Route" - {
+    "when listing entities" - {
+      "returns a JSON list" in {
+        Get("/entity") ~> spraysampleRoute ~> check {
+          assert(contentType.mediaType.isApplication)
+          contentType.toString should include("application/json")
+          // FIXME: Should be able to get response as JObject
+          responseAs[String] should equal("\"list\"")
+        }
       }
     }
-    "Post an entity" in {
-      // FIXME: Should be able to send entity as JObject
-      Post("/entity", HttpEntity(ContentTypes.`application/json`, """{"json":[ ]}""")) ~> spraysampleRoute ~> check {
-	responseAs[String] must contain("\"I got a response: Ok")
+    "when posting an entity" - {
+      "gives a JSON response" in {
+        // FIXME: Should be able to send entity as JObject
+        Post("/entity", HttpEntity(ContentTypes.`application/json`, """{"json":[ ]}""")) ~> spraysampleRoute ~> check {
+          responseAs[String] should include("\"I got a response: Ok")
+        }
       }
     }
-    "Get stats" in {
-      pending
+    "Get stats" - {
+      "gives a JSON response" in {
+        pending
+        // FIXME: Request was neither completed nor rejected within 1
+        // second. Error during processing of request
+        // HttpRequest(GET,http://example.com/stats,List(),Empty,HTTP/1.1)
+        // akka.pattern.AskTimeoutException: Timed out
+        Get("/stats") ~> spraysampleRoute ~> check {
+          responseAs[String] should include("uptime")
+        }
+      }
     }
   }
 }
